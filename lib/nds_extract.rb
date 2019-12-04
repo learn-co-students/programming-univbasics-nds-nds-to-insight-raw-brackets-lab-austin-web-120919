078f0943cb435fed841c775457081fa73d37d15c
Chance Mims <chancemims1@gmail.com>
2019-12-04 02:34:54 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,8 +15,21 @@ def directors_totals(nds)
   # ...
   # ...
   # ...
+  director_index = 0
+  while director_index < nds.length do
+    total_gross = 0
+    movie_index = 0
+    director_name = nds[director_index][:name]
+    while movie_index < nds[director_index][:movies].length do
+      total_gross += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    result[director_name] = total_gross
+    director_index += 1
+  end
   #
   #
   # Be sure to return the result at the end!
+  return result
   nil
 end
